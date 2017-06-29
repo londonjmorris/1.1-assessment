f78ad933e4f7a50c2b9080814146770a0072946e
London Morris <londonjmorris@gmail.com>
2017-06-29 17:02:14 -0400

    initial

---
+++ script.sh
@@ -0,0 +1,10 @@
+mkdir project
+cd project
+touch index.html
+touch styles.css
+touch README.md
+echo "<link rel="stylesheet" href="styles.css"><h1>Hello World!" > index.html
+echo "h1{color:red;font-family:sans-serif;text-decoration:underline;}" > styles.css
+echo "Hello World!" > README.md
+read -p "Press [Enter] key to continue..."
+echo 'Congratulations'
